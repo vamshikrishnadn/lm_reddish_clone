0ffcfcec99680e6b8138c0fc39e7efc17c76180c
Vamshikrishna D N <vamshikrishnadn573@gmail.com>
2025-06-17 22:09:04 +0530

    testing for jenkins

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/post');
 const subredditRoutes = require('./routes/subreddit');
 const userRoutes = require('./routes/user');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/test', (req, res) => {
+  res.status(200).send('<h1>workingg</h1>');
+});
 
 app.use('/api', authRoutes);
 app.use('/api/posts', postRoutes);
@@ -20,5 +27,12 @@ app.use('/api/users', userRoutes);
 
 app.use(middleware.unknownEndpointHandler);
 app.use(middleware.errorHandler);
+
+app.use(express.static(__dirname + '/client'));
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  console.log('hererer');
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 module.exports = app;
